0bc994146ea1aac4e9489d8a829c10045999ab8a
ZuhairBinRedha <hassan121rgc@gmail.com>
2025-03-21 02:19:52 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  // basePath: "/zuhair-smartcard", // for github-pages
+  basePath: "/zuhair-smartcard", // for github-pages
   distDir: "build",
   eslint: {
     ignoreDuringBuilds: true,
